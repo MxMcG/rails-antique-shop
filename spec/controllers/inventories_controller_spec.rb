4dace325e8e518edc154615f1595b27d767a91e1
MxMcG <mmcgeevibes@gmail.com>
2015-10-25 16:55:21 -0700

    add inventory controller changes

--- spec/controllers/inventories_controller_spec.rb
+++ spec/controllers/inventories_controller_spec.rb
@@ -35,22 +35,36 @@ describe InventoriesController do
       expect(assigns[:inventory]).to be_a Item
     end
   end
-  # describe 'post #create' do
-  #   login_admin
-  #   before (:each) do
-  #     get :new
-  #   end
-  #   it 'renders a new inventory form' do
-  #     expect(response).to have_rendered(:form)
-  #   end
-  # end
+
+  describe 'post #create' do
+    login_admin
+    let(:item){Item.new}
+    context 'when creating a new item' do
+      before do
+        expect(Item).to receive(:new).with("name" => "bobby", "description" => "heyheyhey", "price" => "3")
+        expect(item).to receive(:save).and_return(save_return_value)
+        post :create, deck:{"name" => "bobby", "description" => "heyheyhey", "price" => "3"}
+        expect(assigns[:item]).to be Item
+      end
+    end
+  end
   describe 'get #edit' do
+    let(:new_item) {Item.create(name: "check", description: "hey hey hey", price: 5)}
     login_admin
     before (:each) do
-      get :edit
+      get :edit, {name: "check", description: "hey hey hey", price: 3}
+      expect(assigns[:new_item]).to eq(:new)
     end
     it 'renders an edit form' do
       expect(response).to have_rendered(:form)
+    end
+  end
+  describe 'delete #destroy' do
+    let(:destroy_item){Item.create(name: "check", description: "hey hey hey", price: 5)}
+    login_admin
+    it 'destroys an item' do
+      get :destroy, inventory: destroy_item
+      expect(response).to change(Item, :count).by(-1)
     end
   end
 
